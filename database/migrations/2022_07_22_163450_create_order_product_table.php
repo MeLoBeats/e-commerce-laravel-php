9c03db084ff40b69c09c1193d71f1f0e48bd0fb2
MeLoBeats <lamelobeats@gmail.com>
2022-07-22 18:51:41 +0200

    Projet E-Commerce avec migration factory models + seeders  Laravel 9.21.6 et PHP 8.1.1 v0.0.0

---
+++ database/migrations/2022_07_22_163450_create_order_product_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\Order;
+use App\Models\Product;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_product', function (Blueprint $table) {
+            $table->foreignIdFor(Order::class);
+            $table->foreignIdFor(Product::class);
+            $table->integer('total_quantity');
+            $table->integer('total_price');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_product');
+    }
+};
